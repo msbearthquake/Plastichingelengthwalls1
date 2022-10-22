9e4ab0413f34af6a9167cab9aaff6056821909fc
msbearthquake <116441827+msbearthquake@users.noreply.github.com>
2022-10-22 18:31:54 +0330

    Add files via upload

--- streamPHL.py
+++ streamPHL.py
@@ -2,11 +2,17 @@ import   streamlit  as st; from PIL import Image; import numpy  as np
 from  sklearn.ensemble   import   RandomForestRegressor
 import pandas  as pd; import pickle
 
+import os
+
+dirname = os.path.dirname(__file__)
+filename1 = os.path.join(dirname, 'Capture1.png')
+filename2 = os.path.join(dirname, 'Capture2.png')
+
 
 st.title('Plastic hinge length of reinforced concrete structural walls (RCSWs) ')
 with st.container():
-    image1 = Image.open('Capture1.png')
-    image2 = Image.open('Capture2.png')
+    image1 = Image.open(filename1)
+    image2 = Image.open(filename2)
 
     st.image(image1)
     st.image(image2)
@@ -20,7 +26,7 @@ WL2 = WL1*1000.0; HE2 = HE1*1000.0;
 inputvec = np.array([SCR1, WL2, HE2, ALR1, SSP1])
 
 
-filename = 'finalized_model.sav'
+filename3 = os.path.join(dirname, 'finalized_model.sav')
 ### save the model to disk
 # pickle.dump(model, open(filename, 'wb'))
  
@@ -32,7 +38,7 @@ with st.container():
 
     if st.button('Run'):
         if WL2!=0 and HE2!=0:
-            loaded_model = pickle.load(open(filename, 'rb'))
+            loaded_model = pickle.load(open(filename3, 'rb'))
 
             # trainx1 = pd.read_excel('./trainx.xlsx')
             # trainy1 = pd.read_excel('./trainy.xlsx')
@@ -48,10 +54,12 @@ with st.container():
 
 # st.write(trainx)
 
+filename4 = os.path.join(dirname, 'Capture3.png')
+filename5 = os.path.join(dirname, 'Capture4.png')
 st.header("Developers:")
 with st.container():
-    image3 = Image.open('Capture3.png')
-    image4 = Image.open('Capture4.png')
+    image3 = Image.open(filename4)
+    image4 = Image.open(filename5)
 
     st.image(image3)
     st.image(image4)
