47b841dfa268acbdc3b6b3c433d777137e102097
msbearthquake <116441827+msbearthquake@users.noreply.github.com>
2022-10-23 13:42:36 +0330

    Add files via upload

--- streamPHL.py
+++ streamPHL.py
@@ -4,9 +4,8 @@ import pandas  as pd; import pickle
 
 import os
 
-dirname = os.path.dirname(__file__)
-filename1 = dirname + '/Capture1.png'
-filename2 = dirname + '/Capture2.png'
+filename1 = 'https://github.com/msbearthquake/Plastichingelengthwalls1/blob/main/Capture1.PNG'
+filename2 = 'https://github.com/msbearthquake/Plastichingelengthwalls1/blob/main/Capture12.PNG'
 
 
 st.title('Plastic hinge length of reinforced concrete structural walls (RCSWs) ')
@@ -26,7 +25,7 @@ WL2 = WL1*1000.0; HE2 = HE1*1000.0;
 inputvec = np.array([SCR1, WL2, HE2, ALR1, SSP1])
 
 
-filename3 = dirname + '/finalized_model.sav'
+filename3 = 'finalized_model.sav'
 ### save the model to disk
 # pickle.dump(model, open(filename, 'wb'))
  
@@ -54,8 +53,8 @@ with st.container():
 
 # st.write(trainx)
 
-filename4 = dirname + '/Capture3.png'
-filename5 = dirname + '/Capture4.png'
+filename4 = 'https://github.com/msbearthquake/Plastichingelengthwalls1/blob/main/Capture3.PNG'
+filename5 = 'https://github.com/msbearthquake/Plastichingelengthwalls1/blob/main/Capture4.PNG'
 st.header("Developers:")
 with st.container():
     image3 = Image.open(filename4)
