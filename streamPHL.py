ac7fb189eb0824671b7d2baecdcc56ea897c38b1
msbearthquake <116441827+msbearthquake@users.noreply.github.com>
2022-10-23 09:16:44 +0330

    Add files via upload

--- streamPHL.py
+++ streamPHL.py
@@ -5,8 +5,8 @@ import pandas  as pd; import pickle
 import os
 
 dirname = os.path.dirname(__file__)
-filename1 = os.path.join(dirname, 'Capture1.png')
-filename2 = os.path.join(dirname, 'Capture2.png')
+filename1 = dirname + '/Capture1.png'
+filename2 = dirname + '/Capture2.png'
 
 
 st.title('Plastic hinge length of reinforced concrete structural walls (RCSWs) ')
@@ -26,7 +26,7 @@ WL2 = WL1*1000.0; HE2 = HE1*1000.0;
 inputvec = np.array([SCR1, WL2, HE2, ALR1, SSP1])
 
 
-filename3 = os.path.join(dirname, 'finalized_model.sav')
+filename3 = dirname + '/finalized_model.sav'
 ### save the model to disk
 # pickle.dump(model, open(filename, 'wb'))
  
@@ -54,8 +54,8 @@ with st.container():
 
 # st.write(trainx)
 
-filename4 = os.path.join(dirname, 'Capture3.png')
-filename5 = os.path.join(dirname, 'Capture4.png')
+filename4 = dirname + '/Capture3.png'
+filename5 = dirname + '/Capture4.png'
 st.header("Developers:")
 with st.container():
     image3 = Image.open(filename4)
